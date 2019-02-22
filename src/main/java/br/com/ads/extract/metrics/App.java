6df1e8ff22efec9cf42e6a55e0758a316b8586e9
Warteruzannan S. Cunha <warteruzannan@gmail.com>
2019-02-22 13:58:02 -0300

    [FIRST COMMIT]

---
+++ src/main/java/br/com/ads/extract/metrics/App.java
@@ -0,0 +1,13 @@
+package br.com.ads.extract.metrics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
