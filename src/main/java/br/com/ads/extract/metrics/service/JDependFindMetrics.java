741c37239dff762bb9d95cb2d52305c0ec898c58
Warteruzannan S. Cunha <warteruzannan@gmail.com>
2019-03-02 11:23:28 -0300

    ADD: Refatorando o find metrics

--- src/main/java/br/com/ads/extract/metrics/service/JDependFindMetrics.java
+++ src/main/java/br/com/ads/extract/metrics/service/JDependFindMetrics.java
@@ -10,128 +10,8 @@ import java.util.Map;
 import jdepend.framework.JDepend;
 import jdepend.framework.JavaPackage;
 
-public class JDependFindMetrics implements Analyzer{
-		
-	public JDependFindMetrics() {	}
-
-	/*@SuppressWarnings("unchecked")
-	@Override
-	public String findMetrics(String path) {
-		JDepend jDepend = JDependBuilder.builder()
-				.addDirectory(path)
-				.build();
-
-		jDepend.analyze();
-		
-		Iterator<JavaPackage> components = jDepend.getPackages().iterator();		
-		
-		List<Map<Object, Object>> allPackages = new ArrayList<>();
-		Map<Object, Object> packages = new HashMap<>();
-		Map<Object, Object> mestrics = new HashMap<>();
-					
-		List<String> header = Arrays.asList("name",
-											"abstractness",
-											"afferentCoupling",
-											"efferentCoupling",
-											"distance",
-											"instability",
-											"afferents", 
-											"efferents", 
-											"classCount",
-											"volatility",
-											"abstractClassCount", 
-											"concreteClassCount", 
-											"containsCycle",
-											"afferentsPackages",
-											"efferentsPackages");
-							
-		StringBuilder stringBuilder = new StringBuilder();
-				
-		
-		stringBuilder.append(String.join(",", header) + "\n");
-		
-		while(components.hasNext()) {
-			
-			JavaPackage currentPackage = components.next();
-			
-			List<String> componentsInfos = new ArrayList<String>();
-			
-			componentsInfos.add(currentPackage.getName());
-			componentsInfos.add(String.valueOf(currentPackage.abstractness()));
-			componentsInfos.add(String.valueOf(currentPackage.afferentCoupling()));
-			componentsInfos.add(String.valueOf(currentPackage.efferentCoupling()));
-			componentsInfos.add(String.valueOf(currentPackage.distance()));
-			componentsInfos.add(String.valueOf(currentPackage.instability()));
-			componentsInfos.add(String.valueOf(currentPackage.getAfferents().size()));			
-			componentsInfos.add(String.valueOf(currentPackage.getEfferents().size()));
-			componentsInfos.add(String.valueOf(currentPackage.getClassCount()));
-			componentsInfos.add(String.valueOf(currentPackage.getVolatility()));
-			componentsInfos.add(String.valueOf(currentPackage.getAbstractClassCount()));
-			componentsInfos.add(String.valueOf(currentPackage.getConcreteClassCount()));			
-			componentsInfos.add(String.valueOf(currentPackage.containsCycle() ? 1 : 0));
-			
-			componentsInfos.add(this.iteratorToString(currentPackage.getAfferents().iterator()));
-			componentsInfos.add(this.iteratorToString(currentPackage.getEfferents().iterator()));			
-							
-			
-			stringBuilder.append(String.join(",", componentsInfos) + "\n");					
-		}
-		
-		return stringBuilder.toString();
-	}*/
+public class JDependFindMetrics implements Analyzer{			
 	
-	
-	private Map<Object, Object> getMetrics(JavaPackage component){
-		Map<Object, Object> metrics = new HashMap<>();
-		
-		metrics.put("abstractness", component.abstractness());
-		metrics.put("afferentCoupling", component.afferentCoupling());
-		metrics.put("efferentCoupling", component.efferentCoupling());
-		metrics.put("distance", component.distance());
-		metrics.put("instability", component.instability());
-		metrics.put("afferents", component.getAfferents().size());
-		metrics.put("efferents", component.getEfferents().size());
-		metrics.put("volatility", component.getVolatility());
-		metrics.put("abstractClassCount", component.getAbstractClassCount());
-		metrics.put("concreteClassCount", component.getConcreteClassCount());
-		metrics.put("containsCycle", component.containsCycle() ? 1 : 0);		
-		
-		return metrics;
-	}
-	
-	/*private String iteratorToString(Iterator<JavaPackage> iterator) {
-		StringBuilder builder = new StringBuilder();		
-		
-		while(iterator.hasNext()) {
-			JavaPackage next = iterator.next();
-			builder.append(next.getName() + " " + 
-					next.instability() + " " + 
-					next.abstractness() + " " + 
-					next.distance() + " " + 
-					next.afferentCoupling() + " " + 
-					next.efferentCoupling() + "-");
-		} 					
-				
-		return builder.toString();
-	}*/
-	
-	
-	/**
-	 * @param iterator
-	 * @return
-	 */
-	private List<String> getDependenciesFromIterator(Iterator<JavaPackage> iterator) {
-		
-		List<String> relacioships = new ArrayList<>();									
-		
-		while(iterator.hasNext()) {
-			JavaPackage next = iterator.next();
-			relacioships.add(next.getName());			
-		} 					
-				
-		return relacioships;
-	}
-
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Map<Object, Object>> findMetrics(String path) {
@@ -159,6 +39,38 @@ public class JDependFindMetrics implements Analyzer{
 
 		return packages;
 	}
+		
+	private Map<Object, Object> getMetrics(JavaPackage component){
+		Map<Object, Object> metrics = new HashMap<>();
+		
+		metrics.put("abstractness", component.abstractness());
+		metrics.put("afferentCoupling", component.afferentCoupling());
+		metrics.put("efferentCoupling", component.efferentCoupling());
+		metrics.put("distance", component.distance());
+		metrics.put("instability", component.instability());
+		metrics.put("afferents", component.getAfferents().size());
+		metrics.put("efferents", component.getEfferents().size());
+		metrics.put("volatility", component.getVolatility());
+		metrics.put("abstractClassCount", component.getAbstractClassCount());
+		metrics.put("concreteClassCount", component.getConcreteClassCount());
+		metrics.put("containsCycle", component.containsCycle() ? 1 : 0);		
+		
+		return metrics;
+	}
+		
+		
+	private List<String> getDependenciesFromIterator(Iterator<JavaPackage> iterator) {
+		
+		List<String> relacioships = new ArrayList<>();									
+		
+		while(iterator.hasNext()) 			
+			relacioships.add(iterator.next().getName());			
+		
+				
+		return relacioships;
+	}
+
+	
 	
 
 }
--- src/test/java/br/com/ads/extract/metrics/JDependFindMetricsTest.java
+++ src/test/java/br/com/ads/extract/metrics/JDependFindMetricsTest.java
@@ -25,7 +25,7 @@ public class JDependFindMetricsTest {
 		assertEquals(result.size(), 6);		
 	}
 	
-	
+	// FIXME: Teste redundante, remover..
 	@Test
 	public void assertFoundComponetsFromProjectDependenciesTest() {						
 		List<Map<Object, Object>> result  = this.jDependFindMetrics.findMetrics("./src/test/resources/dependencies");		
@@ -41,13 +41,21 @@ public class JDependFindMetricsTest {
 		}
 	}
 	
+	
 	@Test
 	public void assertMetricsFromProjectDEpendenciesTest() {
 		List<Map<Object, Object>> componets  = this.jDependFindMetrics.findMetrics("./src/test/resources/dependencies");
 		
-		String expectedResult = "{name=br.com.dependencies.core, dependents=[br.com.dependencies.services, br.com.dependencies.net], metrics={efferentCoupling=2, concreteClassCount=7, afferentCoupling=2, distance=0.5, instability=0.5, afferents=2, efferents=2, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[br.com.dependencies.utils, br.com.dependencies.data]}";		
-		String result = componets.get(0).toString();
 		
-		assertEquals(expectedResult,result);
+		String[] expectedsResult = {"{name=br.com.dependencies.core, dependents=[br.com.dependencies.services, br.com.dependencies.net], metrics={efferentCoupling=2, concreteClassCount=7, afferentCoupling=2, distance=0.5, instability=0.5, afferents=2, efferents=2, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[br.com.dependencies.utils, br.com.dependencies.data]}",
+									"{name=br.com.dependencies.utils, dependents=[br.com.dependencies.core], metrics={efferentCoupling=0, concreteClassCount=2, afferentCoupling=1, distance=1.0, instability=0.0, afferents=1, efferents=0, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[]}",
+									"{name=br.com.dependencies.net, dependents=[br.com.dependencies.services], metrics={efferentCoupling=1, concreteClassCount=2, afferentCoupling=1, distance=0.5, instability=0.5, afferents=1, efferents=1, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[br.com.dependencies.core]}",
+									"{name=br.com.dependencies.data, dependents=[br.com.dependencies.core], metrics={efferentCoupling=0, concreteClassCount=3, afferentCoupling=1, distance=1.0, instability=0.0, afferents=1, efferents=0, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[]}",
+									"{name=br.com.dependencies.services, dependents=[br.com.dependencies.ui], metrics={efferentCoupling=2, concreteClassCount=3, afferentCoupling=1, distance=0.3333333, instability=0.6666667, afferents=1, efferents=2, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[br.com.dependencies.core, br.com.dependencies.net]}",
+									"{name=br.com.dependencies.ui, dependents=[], metrics={efferentCoupling=1, concreteClassCount=1, afferentCoupling=0, distance=0.0, instability=1.0, afferents=0, efferents=1, volatility=1, abstractClassCount=0, containsCycle=0, abstractness=0.0}, dependencies=[br.com.dependencies.services]}"};
+		
+		for(int i = 0; i < componets.size(); i++) {
+			assertEquals(componets.get(i).toString(),expectedsResult[i]);
+		}			
 	}
 }
